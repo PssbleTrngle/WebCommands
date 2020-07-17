d6a53cc78d6509a2076a62daa3be200e14839d30
PssbleTrngle <nik@somethingcatchy.net>
2020-07-17 10:38:53 +0200

    Update loom version

--- src/main/kotlin/com/possible_triangle/webcommands/WebCommands.kt
+++ src/main/kotlin/com/possible_triangle/webcommands/WebCommands.kt
@@ -1,7 +1,6 @@
 package com.possible_triangle.webcommands
 
 import net.fabricmc.api.DedicatedServerModInitializer
-import net.fabricmc.fabric.api.event.server.ServerStartCallback
 
 @Suppress("unused")
 class WebCommands : DedicatedServerModInitializer  {
